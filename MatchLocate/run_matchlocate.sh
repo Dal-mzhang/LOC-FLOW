591137de9a0c57318e4c47f8b5cdce9b5cfc7156
Miao Zhang <51533859+Dal-mzhang@users.noreply.github.com>
2021-08-03 00:09:16 -0300

    Update run_matchlocate.sh

--- MatchLocate/run_matchlocate.sh
+++ MatchLocate/run_matchlocate.sh
@@ -5,13 +5,12 @@
 #step 1#
 #convert the catalog format  (as template)
 #only keep events with large mag (e.g., 0.5)
-cat ../GrowClust/OUT/out.growclust_cat |  awk '{if($11 > -1.5) printf"%4d/%02d/%02d %02d:%02d:%05.2f %7.4f %8.4f %6.2f M %5.2f\n",$1,$2,$3,$4,$5,$6,$8,$9,$10,$11}' > catalog_all.dat
+cat ../GrowClust/OUT/out.growclust_cat |  awk '{if($11 > 0.5) printf"%4d/%02d/%02d %02d:%02d:%05.2f %7.4f %8.4f %6.2f M %5.2f\n",$1,$2,$3,$4,$5,$6,$8,$9,$10,$11}' > catalog_all.dat
 #cat ../hypoDD_dtcc/hypoDD.reloc |  awk '{if($17 > 0.5) printf"%4d/%02d/%02d %02d:%02d:%05.2f %7.4f %8.4f %6.2f M %5.2f\n",$11,$12,$13,$14,$15,$16,$2,$3,$4,$17}' > catalog_all.dat
 #cat ../hypoDD_dtct/hypoDD.reloc |  awk '{if($17 > 0.5) printf"%4d/%02d/%02d %02d:%02d:%05.2f %7.4f %8.4f %6.2f M %5.2f\n",$11,$12,$13,$14,$15,$16,$2,$3,$4,$17}' > catalog_all.dat
 #cat ../REAL/catalogSA_allday.txt  |  awk '{if($10 > 0.5) printf"%4d/%02d/%02d %02d:%02d:%05.2f %7.4f %8.4f %6.2f M %5.2f\n",$1,$2,$3,$4,$5,$6,$7,$8,$9,$10}' > catalog_all.dat 
-cat catalog_all.dat > catalog.dat # Use the first three templates as an example, not test the whole catalog.
-
-#head -10 catalog_all.dat > catalog.dat # Use the first three templates as an example, not test the whole catalog.
+#cat catalog_all.dat > catalog.dat 
+head -10 catalog_all.dat > catalog.dat # Use the first ten templates as an example
 
 #step 2#
 #cut waveforms for templates, mark their P and S arrivals
