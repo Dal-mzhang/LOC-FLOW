5f75c2732b56122c3c514610cd13e51322fa1dd6
Miao Zhang <51533859+Dal-mzhang@users.noreply.github.com>
2021-08-02 23:26:46 -0300

    Update software_download.py

--- src/software_download.py
+++ src/software_download.py
@@ -1,14 +1,14 @@
 import warnings
 import os
 
-#os.system('git clone https://github.com/Dal-mzhang/REAL.git')
-#os.system('git clone https://github.com/wayneweiqiang/PhaseNet.git') #Note: it is a new version
-#os.system('wget http://www.ldeo.columbia.edu/~felixw/HYPODD/HYPODD_1.3.tar.gz -N')
-#os.system('tar -zxvf HYPODD_1.3.tar.gz')
-#os.system('rm HYPODD_1.3.tar.gz')
-#os.system('git clone https://github.com/dttrugman/GrowClust.git')
+os.system('git clone https://github.com/Dal-mzhang/REAL.git')
+os.system('git clone https://github.com/wayneweiqiang/PhaseNet.git') #Note: it is a new version
+os.system('wget http://www.ldeo.columbia.edu/~felixw/HYPODD/HYPODD_1.3.tar.gz -N')
+os.system('tar -zxvf HYPODD_1.3.tar.gz')
+os.system('rm HYPODD_1.3.tar.gz')
+os.system('git clone https://github.com/dttrugman/GrowClust.git')
 os.system('git clone https://github.com/Dal-mzhang/MatchLocate2.git')
-#os.system('git clone https://github.com/MinLiu19/FDTCC')
+os.system('git clone https://github.com/MinLiu19/FDTCC')
 
 
 #hypoinverse can be downloaded at https://www.usgs.gov/software/hypoinverse-earthquake-location
