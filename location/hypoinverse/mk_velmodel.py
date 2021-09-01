29cdabda18ea77e99050ee8f3e1cc7def699f939
Miao Zhang <51533859+Dal-mzhang@users.noreply.github.com>
2021-09-01 14:21:25 -0300

    Add files via upload
    include the upper mantle layer in velocity model

--- location/hypoinverse/mk_velmodel.py
+++ location/hypoinverse/mk_velmodel.py
@@ -1,14 +1,20 @@
 import os
 import sys
 import datetime
+import linecache
 
 #Here we didn't consider depth above the sea level to avoid negative depth
 #velocity model is relative to the average of station elevation
+
+def get_line_context(file_path, line_number):
+    return linecache.getline(file_path, line_number).strip()
+
 def model_format(modelin):
     output = 'vel_model_P.crh'  # velocity model
     gg = open(output, 'w')
     gg.write("MODEL Vp from REAL\n")
     kk = 0
+    i=0
     with open(modelin, "r") as f:
         for line in f:
             line = line.strip('\n')
@@ -19,6 +25,14 @@ def model_format(modelin):
                 dep = float(dep)
                 vp = float(line.split()[1])
                 gg.write('{:4.2f}  {:5.2f}\n'.format(vp, dep))
+                i=i+1
+    line_more = get_line_context(modelin, i+2)
+    dep = line_more.split()[0]
+    dep_1 = float(dep) + 0.1 # HYPOINVERSE doesn't like the same depth
+    vp_1 = float(line_more.split()[1])
+    vs_1 = float(line_more.split()[2])
+    gg.write('{:4.2f}  {:5.2f}\n'.format(vp_1, dep_1)) # include the upper mantle layer
+
     output = 'vel_model_S.crh'  # velocity model
     gg = open(output, 'w')
     gg.write("MODEL Vs from REAL\n")
@@ -33,6 +47,7 @@ def model_format(modelin):
                 dep = float(dep)
                 vs = float(line.split()[2])
                 gg.write('{:4.2f}  {:5.2f}\n'.format(vs, dep))
+    gg.write('{:4.2f}  {:5.2f}\n'.format(vs_1, dep_1)) # include the upper mantle layer 
         
 if __name__ == '__main__':
     if len(sys.argv) != 2:
