33c2e328d5452d073ad28c2703806dc4291ed1bd
Miao Zhang <51533859+Dal-mzhang@users.noreply.github.com>
2021-09-06 12:12:05 -0300

    Add files via upload

--- Data/phasenet_input.py
+++ Data/phasenet_input.py
@@ -26,6 +26,9 @@ dayleng = 3000 # seconds in one day
 latref = 42.75 # reference lat.
 lonref = 13.25 # reference lon.
 maxradius = 50 # maximum radius in km.
+numberofcomp = 3 # 1: use either Z or E,N,Z (PhaseNet works for either one)
+                 # 3: use E,N,Z (STA/LTA requires three components)
+                 #    current FDTCC requires two horizontal components to calculate dtcc for S phase pairs
 
 data_dir = os.getcwd()
 sac_waveform_dir = os.path.join(data_dir, "waveform_sac")
@@ -56,8 +59,8 @@ for i in range(nday):
         for station in f:
             lon, lat, net, sta, chan, elev = station.split(" ")
         
-            chane = chan[:2]+"E"
-            chann = chan[:2]+"N"
+            chane = chan[:2]+"E" #E,2
+            chann = chan[:2]+"N" #N,1 consider use st.rotate in waveform_download_mseed.py
             chanz = chan[:2]+"Z"
 
             tracee = os.path.join(sacid_dir,net+'.'+sta+'.'+chane)
@@ -67,30 +70,48 @@ for i in range(nday):
             dist = 111.19*locations2degrees(float(latref), float(lonref), float(lat), float(lon))
             if dist > maxradius:
                 continue
-
-            if not os.path.exists(tracee) or (not os.path.exists(tracen)) or (not os.path.exists(tracez)):
+            
+            if not os.path.exists(tracez):
                 continue
+
+            if os.path.exists(tracee) and os.path.exists(tracen) and os.path.exists(tracez):
             
-            meta = obspy.Stream()
+                meta = obspy.Stream()
             
-            # Although PhaseNet was trained using raw data without filtering,
-            # highpass filtered waveforms have a better performace.
-            tre = read(tracee)
-            tre.detrend('demean')
-            tre.detrend('linear')
-            tre.filter(type="highpass",freq=1.0,zerophase=False)
+                # Although PhaseNet was trained using raw data without filtering,
+                # highpass filtered waveforms have a better performace.
+                tre = read(tracee)
+                tre.detrend('demean')
+                tre.detrend('linear')
+                tre.filter(type="highpass",freq=1.0,zerophase=False)
             
-            trn = read(tracen)
-            trn.detrend('demean')
-            trn.detrend('linear')
-            trn.filter(type="highpass",freq=1.0,zerophase=False)
+                trn = read(tracen)
+                trn.detrend('demean')
+                trn.detrend('linear')
+                trn.filter(type="highpass",freq=1.0,zerophase=False)
             
-            trz = read(tracez)
-            trz.detrend('demean')
-            trz.detrend('linear')
-            trz.filter(type="highpass",freq=1.0,zerophase=False)
+                trz = read(tracez)
+                trz.detrend('demean')
+                trz.detrend('linear')
+                trz.filter(type="highpass",freq=1.0,zerophase=False)
+            
+                meta = tre + trn + trz
+            else:
                 
-            meta = tre + trn + trz
+                if numberofcomp == 1:
+                    meta = obspy.Stream()
+
+                    trz = read(tracez)
+                    trz.detrend('demean')
+                    trz.detrend('linear')
+                    trz.filter(type="highpass",freq=1.0,zerophase=False)
+                    
+                    meta = trz
+                elif numberofcomp == 3:
+                    continue
+                else:
+                    print('use vertical component or three components? try numberofcomp=1 or 3')
+
             tb = UTCDateTime(int(year),int(mon),int(day))
             te = tb + dayleng
             meta = meta.trim(tb, te, pad=True, fill_value=0)
